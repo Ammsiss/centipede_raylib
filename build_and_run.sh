8643fc6322f2004950d455c46a3df3ad22fc915b
Ammsiss <j7nj1.t41@gmail.com>
2023-12-10 16:37:02 -0400

    updated file path

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESOURCE_PATH="$HOME/Pictures/Sprites/mushroom.png"
+RESOURCE_PATH="$(dirname "$0")/assets/sprites/mushroom.png"
 
 g++ -o main src/main.cpp -lraylib -ldl -lpthread -lm -lX11
 ./main "$RESOURCE_PATH"
